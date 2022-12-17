6668bcb394e137c47adba9cb45f266d5a58b92a0
0xHexPloit <78966024+0xHexPloit@users.noreply.github.com>
2022-12-17 12:29:08 +0100

    Adding banner

--- xeely/__init__.py
+++ xeely/__init__.py
@@ -9,5 +9,6 @@ project_data = toml.load(str(project_file_path))
 
 __app_name__ = project_data["tool"]["poetry"]["name"]
 __app_version__ = project_data["tool"]["poetry"]["version"]
+__author__ = project_data["tool"]["poetry"]["authors"][0].split(" ")[0]
 
 RESOURCES_PATH = root_dir / "xeely" / "resources"
--- xeely/__main__.py
+++ xeely/__main__.py
@@ -1,6 +1,28 @@
 from xeely import __app_name__
+from xeely import __app_version__
+from xeely import __author__
 from xeely import cli
 from xeely import console
+
+
+def print_banner():
+    banner = f"""
+$$\\   $$\\                     $$\
+$$ |  $$ |                    $$ |
+\\$$\\ $$  | $$$$$$\\   $$$$$$\\  $$ |$$\\   $$\
+ \\$$$$  / $$  __$$\\ $$  __$$\\ $$ |$$ |  $$ |
+ $$  $$<  $$$$$$$$ |$$$$$$$$ |$$ |$$ |  $$ |
+$$  /\\$$\\ $$   ____|$$   ____|$$ |$$ |  $$ |
+$$ /  $$ |\\$$$$$$$\\ \\$$$$$$$\\ $$ |\\$$$$$$$ |
+\\__|  \\__| \\_______| \\_______|\\__| \\____$$ |
+                                  $$\\   $$ |
+                                  \\$$$$$$  |
+                                   \\______/
+
+
+by {__author__} v{__app_version__}
+    """
+    print(banner)
 
 
 def main():
@@ -9,6 +31,7 @@ def main():
 
 if __name__ == "__main__":
     try:
-        main()
+        print_banner()
+        # main()
     except Exception as e:
         console.print_error(str(e))
